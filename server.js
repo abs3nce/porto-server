89dde2e72e8ba0da192fe6c9e5c090999ea55bd3
abs3nce <vodrazka.jakub@gmail.com>
2021-03-16 21:41:33 +0100

    - db connection, base server done

---
+++ server.js
@@ -0,0 +1,31 @@
+//import libiek a packagov
+const express = require("express"); //framework na pracu s requestami
+const mongoose = require("mongoose"); //libka na spojenie sa s mongoDB
+const cors = require("cors"); //dovoluje ostatnym strankam robit requesty na toto API
+require("dotenv/config"); //prepojenie .env kde je link na connection s databazou so server.js
+
+//pripojenie na db
+mongoose.connect(
+    process.env.DB_CONNECTION,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => {
+        console.log("> Connected to DB");
+    }
+);
+
+const app = express(); //cez app teraz viem pouzivat express
+
+//middleware - zapnutie corsu a body-parseru (integrovany v expresse)
+app.use(cors());
+app.use(express.json());
+
+//import routov
+
+//middleware na pouzivanie tychto externych routes
+
+//server listener
+port = process.env.port || 3000;
+app.listen(port, (err) => {
+    if (err) console.log(err);
+    console.log("> Server running on port: ", port);
+});
