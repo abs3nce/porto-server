74ba00b45c1114d8cc31fcf681dbfca865090354
abs3nce <vodrazka.jakub@gmail.com>
2021-04-11 10:47:21 +0200

    - logging for validation

--- routes/route_verifylogin.js
+++ routes/route_verifylogin.js
@@ -7,22 +7,28 @@ const User = require("../models/model_user");
 
 router.get("/", (req, res, next) => {
     let token = req.headers.token; //tuto ziskavam vlastne token z clienta
+    console.log("\n>> REQUEST TO VERIFY LOGIN:\n", token + "\n");
 
     jwt.verify(
         token,
         "mojsupermegaultragigatutifrutisecretkey",
         (err, decoded) => {
             //pokial nie je token validny tak rejectni request s tym ze uzivatel nie je opravneny
-            if (err)
+            if (err) {
+                console.log(">> USER TOKEN INVALID\n");
                 return res.status(401).json({
                     title: "Unauthorized access, please sign in",
+                    description: "Invalid user token",
                     error: err,
                 });
+            }
 
+            console.log(">> USER TOKEN VALID\n");
             //pokial je token valid tak preposli na clienta spravne data o uzivatelovi
             User.findOne({ _id: decoded.userID }, (err, user) => {
-                if (err)
-                    return res.status(401).json({ title: "User not verified" });
+                if (err) return res.status(500).json({ title: "Server error" });
+
+                console.log(">> USER FOUND:\n", user);
                 return res.status(200).json({
                     title: "User info",
                     userData: { username: user.username, _id: user._id },
